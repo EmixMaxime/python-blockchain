5b019e7ffaf38be93552f2e5855efc392a1f6c87
mx <emixmaxime@outlook.fr>
2019-10-30 12:59:44 +0100

    wip clean(wallet)

--- wallet.py
+++ wallet.py
@@ -5,6 +5,7 @@ from Crypto import Random
 from base64 import b64encode, b64decode
 import hashlib
 from Crypto.Hash import SHA256
+from mx_crypto import MxCrypto
 
 
 class Wallet:
@@ -17,27 +18,15 @@ class Wallet:
             public_key, private_key, str_public_key = Wallet._import_keys()
         except FileNotFoundError:
             # to handle!
-            public_key, private_key = Wallet.generate_keys()
+            public_key, private_key = MxCrypto.generate_keys()
+
+            Wallet._save_key(public_key, private_key)
 
         self.public_key = public_key
         self.private_key = private_key
 
-        self.address = hashlib.sha256(
-            str_public_key.encode('utf-8')).hexdigest()
-
-    @staticmethod
-    def generate_keys():
-        public_key, private_key = Wallet._new_keys(2048)
-        Wallet._save_key(public_key, private_key)
-
-        return public_key, private_key
-
-    @staticmethod
-    def _new_keys(keysize):
-        private_key = RSA.generate(keysize)
-        public_key = private_key.publickey()
-
-        return public_key, private_key
+        # self.address = hashlib.sha256(
+        #     str_public_key.encode('utf-8')).hexdigest()
 
     @staticmethod
     def _save_key(public_key, private_key):
@@ -51,14 +40,6 @@ class Wallet:
         with open('./keys/public.pem', 'w') as pu:
             pu.write(public_pem)
 
-        # DOESN'T WORK
-        # Thanks https://stackoverflow.com/questions/9197507/saving-rsa-keys-to-a-file-using-pycrypto
-        # with open("./keys/private.pem", "w") as prv_file:
-        #     print("{}".format(private_key.exportKey(format='PEM')), file=prv_file)
-
-        # with open("./keys/public.pem", "w") as pub_file:
-        #     print("{}".format(public_key.exportKey(format='PEM')), file=pub_file)
-
     @staticmethod
     def _import_keys():
         """
@@ -71,22 +52,12 @@ class Wallet:
 
         return public_key, private_key, str_public_key
 
-    def encrypt(self, message):
-        # Instantiating PKCS1_OAEP object with the public key for encryption
-        cipher = PKCS1_OAEP.new(self.public_key)
-        return cipher.encrypt(message)
+    def sign_transaction(self, transaction):
+        """
+        Sign transaction with private key
+        """
 
-    def decrypt(self, encrypted_message):
-        # Instantiating PKCS1_OAEP object with the private key for decryption
-        decrypt = PKCS1_OAEP.new(key=self.private_key)
-        return decrypt.decrypt(encrypted_message)
+        if not isinstance(transaction):
+            raise ValueError('transaction should be Transation instance.')
 
-    def sign(self, bytes):
-        signer = PKCS1_v1_5.new(self.private_key)
-        digest = SHA256.new(bytes)
-        return signer.sign(digest)
-
-    def verify(self, message, signature, pub_key):
-        signer = PKCS1_v1_5.new(pub_key)
-        digest = SHA256.new(message)
-        return signer.verify(digest, signature)
+        return Wallet.sign(str(transaction.to_dict()), self.private_key)
