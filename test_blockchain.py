80e1d2c28a13f96575c3c408d87c9bf2f3c1249f
mx <emixmaxime@outlook.fr>
2019-11-02 16:24:06 +0100

    test(blockchain)

--- test_blockchain.py
+++ test_blockchain.py
@@ -25,3 +25,22 @@ def test_mine():
         blockchain.submit_transaction(transaction, signature)
 
     print(blockchain.mine())
+
+
+def test_chain_for_network():
+    for i in range(3):
+        transaction, signature = Client.generate_transaction(
+            wallet, guy_wallet.address, 10)
+        blockchain.submit_transaction(transaction, signature)
+
+    blockchain.mine()
+
+    s = blockchain.chain_for_network
+    assert isinstance(s, str) == True
+
+
+def test_valid_chain():
+    s = blockchain.chain_for_network
+    valid = blockchain.valid_chain(s)
+
+    assert valid == True
