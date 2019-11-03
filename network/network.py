4b847ef59bef46536e31619f238fcee9c2e6dda3
mx <emixmaxime@outlook.fr>
2019-11-03 19:57:51 +0100

    improve(network and node): self discover for myself

--- network/network.py
+++ network/network.py
@@ -3,78 +3,79 @@ import jsonpickle
 from threading import Thread
 from node import Node
 
+
 class Network:
-  
-	def __init__(self, Node_):  
-		self.node = node_
-		self.nodes = []
-		self.blockchain = None
-		node.init_node()
 
-	def set_blockchain(self, blockchain_): #Done
-		self.blockchain = blockchain_
+    def __init__(self):
+        self.node = Node()
+        self.nodes = []
+        self.blockchain = None
 
-	def broadcast_transaction(self, str_transaction_): #Done
-		for nodeList in self.nodes:
-			self.node.send("-t ", node.s, nodeList, str_transaction_)
-    
-	def broadcast_block(self, str_block_): #Done
-		for nodeList in self.nodes:
-			self.node.send("-b ", node.s, nodeList, str_block_)
+    def set_blockchain(self, blockchain_):  # Done
+        self.blockchain = blockchain_
 
-	def broadcast_ask_chain(self): #Done
-		for nodeList in self.nodes:
-			self.node.send("-c ", node.s, nodeList, "")
+    def broadcast_transaction(self, str_transaction_):  # Done
+        for nodeList in self.nodes:
+            self.node.send("-t ", node.s, nodeList, str_transaction_)
 
-	def broadcast_ping(self):
-		nodeBroadcast = node("255.255.255.0")
-		self.node.send("-p ", self.node.s, nodeBroadcast, "") 
+    def broadcast_block(self, str_block_):  # Done
+        for nodeList in self.nodes:
+            self.node.send("-b ", node.s, nodeList, str_block_)
 
-	def receiv(self):
-		print("ready to receiv")
-    
-		while True:
-			data, addr = mode.s.recvfrom(1024)
-			curentNode = node(str(addr))
+    def broadcast_ask_chain(self):  # Done
+        for nodeList in self.nodes:
+            self.node.send("-c ", node.s, nodeList, "")
 
-			myData = str(data)
+    def broadcast_ping(self):
+        nodeBroadcast = Node("255.255.255.0")
+        self.node.send("-p ", self.node.s, nodeBroadcast, "")
 
-			if myData[:3] == "-c ": #Done
-				#Retourne le JSON de la chaine
-				self.node.send("-ac", self.node.s, cureNode, self.blockchain.chain_for_network)
+    def receiv(self):
+        print("ready to receiv")
 
-			elif myData[:3] == "-n ": #Done
-				#Parcourir la liste de noeud et les envois a l'emmeteur
-				for nodeList in self.nodes:
-					nodeToSend = jsonpickle.encode(nodeList)
-					self.node.send("-an", self.node.s, cureNode, nodeToSend)
+        while True:
+            data, addr = mode.s.recvfrom(1024)
+            curentNode = Node(str(addr))
 
-			elif myData[:3] == "-t ": #Done
-				#Reception d'une transaction
-				self.blockchain.submit_block(myData[3:length(myData)])
+            myData = str(data)
 
-			elif myData[:3] == "-b ": #Done
-				#Reception d'un block
-				self.blockchain.submit_transaction(myData[3:length(myData)])
+            if myData[:3] == "-c ":  # Done
+                # Retourne le JSON de la chaine
+                self.node.send("-ac", self.node.s, cureNode,
+                               self.blockchain.chain_for_network)
 
-			elif myData[:3] == "-p ": #Done
-				#Repond present 
-				nodeToSend = jsonpickle.encode(self.node)
-				self.node.send("-an", self.node, cureNode, nodeToSend)
+            elif myData[:3] == "-n ":  # Done
+                # Parcourir la liste de noeud et les envois a l'emmeteur
+                for nodeList in self.nodes:
+                    nodeToSend = jsonpickle.encode(nodeList)
+                    self.node.send("-an", self.node.s, cureNode, nodeToSend)
 
-			elif myData[:3] == "-ac": #En suspend
-				some = None 
+            elif myData[:3] == "-t ":  # Done
+                # Reception d'une transaction
+                self.blockchain.submit_block(myData[3:length(myData)])
 
-			elif myData[:3] == "-an": #Done
-				nodeReceiv = jsonpickle.decode(myData[3:length(myData)])
+            elif myData[:3] == "-b ":  # Done
+                # Reception d'un block
+                self.blockchain.submit_transaction(myData[3:length(myData)])
 
-				notFind = True
-				for nodeList in self.nodes:
-					if nodeList.host == nodeReceiv.host:
-						notFind = False
+            elif myData[:3] == "-p ":  # Done
+                # Repond present
+                nodeToSend = jsonpickle.encode(self.node)
+                self.node.send("-an", self.node, cureNode, nodeToSend)
 
-				if notFind:
-					self.nodes.append(nodeReceiv)
-					notFind = False
-		
-		c.close()
+            elif myData[:3] == "-ac":  # En suspend
+                some = None
+
+            elif myData[:3] == "-an":  # Done
+                nodeReceiv = jsonpickle.decode(myData[3:length(myData)])
+
+                notFind = True
+                for nodeList in self.nodes:
+                    if nodeList.host == nodeReceiv.host:
+                        notFind = False
+
+                if notFind:
+                    self.nodes.append(nodeReceiv)
+                    notFind = False
+
+        c.close()
--- network/node.py
+++ network/node.py
@@ -1,19 +1,35 @@
 import socket
 from threading import Thread
 
+
 class Node:
-	def __init__(self, host_):
-		self.host = host_ #Adresse du pc
-		self.port = 5000
-		self.s = None
+    """
+    if host is none, so it's me -> auto discover of my local ip address.
+    """
 
-	def init_node(self):
-		self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-		self.s.bind((self.host, self.port))
+    def __init__(self, host_=None):
 
-	def ping(self, node_to_ping): 
-		send("-p ", self.s, node_to_ping, "");
+        if host_ is Node:
+            host_name = socket.gethostname()
+            self.host = socket.gethostbyname(host_name)
+            self._init_node()
+        else:
+            self.host = host_  # Adresse du pc
 
-	def send(self, command_, s_, node_, message_):
-		server = (node_.host, node_.port) #Server 
-		self.s_.sendto(command_ + message_, servers)
+        self.port = 5000
+        self.s = None
+
+    def _init_node(self):
+    """
+    Initialize the node as myself.
+    """
+
+    self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    self.s.bind((self.host, self.port))
+
+    def ping(self, node_to_ping):
+        send("-p ", self.s, node_to_ping, "")
+
+    def send(self, command_, s_, node_, message_):
+        server = (node_.host, node_.port)  # Server
+        self.s_.sendto(command_ + message_, servers)
