b130635031dae69a666378ac9b80aa84fe96a170
LDE <lorianedehu@gmail.com>
2019-11-04 23:30:19 +0100

    Init transactions

--- init_transactions.py
+++ init_transactions.py
@@ -1,5 +1,10 @@
 from transaction import Transaction
 import hashlib
+from init_datas import creator_address
+from init_datas import tx_signatures
+
+#from wallet import Wallet
+#creator_wallet = Wallet(); creator_address = creator_wallet.address; print(creator_address)
 
 def getInitialTransactions():
     txs = []
@@ -8,16 +13,19 @@ def getInitialTransactions():
     ironSwordHash = hashImg('./items/iron_sword.png')
 
     for i in range(4):
-        tx = Transaction('@sender_address', '@recipient_address', appleHash)
-        #tx.sign(wallet)
+        tx = Transaction('', creator_address, appleHash)
+        tx.signature = bytes.fromhex(tx_signatures[i*3])
+        #tx.sign(creator_wallet); print("sign#", i*3, tx.signature.hex())
         txs.append(tx)
 
-        tx = Transaction('@sender_address', '@recipient_address', ironAxeHash)
-        #tx.sign(wallet)
+        tx = Transaction('', creator_address, ironAxeHash)
+        tx.signature = bytes.fromhex(tx_signatures[i*3 + 1])
+        #tx.sign(creator_wallet); print("sign#", i*3 + 1, tx.signature.hex())
         txs.append(tx)
 
-        tx = Transaction('@sender_address', '@recipient_address', ironSwordHash)
-        #tx.sign(wallet)
+        tx = Transaction('', creator_address, ironSwordHash)
+        tx.signature = bytes.fromhex(tx_signatures[i*3 + 2])
+        #tx.sign(creator_wallet); print("sign#", i*3 + 2, tx.signature.hex())
         txs.append(tx)
 
     return txs
