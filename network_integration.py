8a5efad1418372c159084eda49a8d4b685eba4b1
soso0084 <du8384@hotmail.fr>
2019-11-04 22:23:21 +0100

    Test

--- network_integration.py
+++ network_integration.py
@@ -1,4 +1,14 @@
 from blockchain_factory import blockchain_factory
+from wallet import Wallet
+from transaction import Transaction
 
 
 bc, network = blockchain_factory()
+
+wallet = Wallet()
+wallet2 = Wallet(1024, True)
+transaction = Transaction(wallet.addresse, wallet2.addresse, "bonjour")
+
+transaction.sign(wallet)
+
+bc.submit_transaction(transaction)
