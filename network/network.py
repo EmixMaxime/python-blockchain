511e1aab17cc938baa6f93ffa088776a4c382e62
soso0084 <du8384@hotmail.fr>
2019-11-05 19:47:15 +0100

    up size of buffer

--- network/network.py
+++ network/network.py
@@ -44,7 +44,7 @@ class Network:
         print("ready to receiv")
 
         while self._running is True:
-            data, addr = self.node.my_socket.recvfrom(2048)
+            data, addr = self.node.my_socket.recvfrom(4096)
 
             cureNode = addr
 
