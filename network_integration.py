c02f0b98ae6127dff852933557af4edb7de864ea
EmixMaxime <contact@maxime-moreau.fr>
2019-11-05 22:03:25 +0100

    feat(network_integration): send real things.

--- network_integration.py
+++ network_integration.py
@@ -1,21 +1,34 @@
 from blockchain_factory import blockchain_factory
 from wallet import Wallet
 from transaction import Transaction
+from init_transactions import hashImg
 import time
+
+appleHash = hashImg('./items/apple.png')
 
 
 bc, network = blockchain_factory()
 
 time.sleep(5)
 
-wallet = Wallet()
-wallet2 = Wallet(1024, True)
-transaction = Transaction(wallet.address, wallet2.address, "bonjour")
-transaction2 = Transaction(wallet.address, wallet2.address, "bonjour2")
-transaction3 = Transaction(wallet.address, wallet2.address, "bonjour3")
+wallet = Wallet(testDatas=True)
+wallet2 = Wallet(testDatas=True)
+
+transaction = Transaction(wallet.address, wallet2.address, appleHash)
+transaction2 = Transaction(wallet.address, wallet2.address, appleHash)
+transaction3 = Transaction(wallet.address, wallet2.address, appleHash)
 
 transaction.sign(wallet)
+transaction2.sign(wallet)
+transaction3.sign(wallet)
 
 bc.submit_transaction(transaction)
 bc.submit_transaction(transaction2)
 bc.submit_transaction(transaction3)
+
+print("transactions:")
+print(bc.current_transactions)
+
+time.sleep(15)
+print("transactions:")
+print(bc.current_transactions)
